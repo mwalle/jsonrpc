e13f03b692f5396b9d2615cb9704cdfd61e3f50a
Michael Walle <michael@walle.cc>
2015-01-27 21:56:01 +0100

    new test suite
    Instead of using python and nosetest, we use a simpler approach. We read
    the input from a file, write the output to a file and compare the output
    with a predefined output.
    
    This is idea is stolen from libjansson :)

--- test/handle_stdio.c
+++ test/handle_stdio.c
@@ -37,9 +37,31 @@ static jsonrpc_ret_t add(json_t *params)
 }
 jsonrpc_register(add);
 
+static jsonrpc_ret_t subtract(json_t *params)
+{
+	json_error_t err;
+	int rc, a, b;
+
+	rc = json_unpack_ex(params, &err, 0, "[ii]", &a, &b);
+	if (rc) {
+		rc = json_unpack_ex(params, &err, 0, "{s:i,s:i}",
+				"minuend", &a,
+				"subtrahend", &b);
+	}
+	if (rc) {
+		return jsonrpc_error_invalid_params(json_string(err.text));
+	}
+	return jsonrpc_result(json_integer(a-b));
+}
+jsonrpc_register(subtract);
+
 int main()
 {
 	char *buf;
+
+	jsonrpc_config_set(
+			  JSONRPC_DISABLE_ERROR_TEXT
+			| JSONRPC_ORDERED_RESPONSE);
 
 	buf = jsonrpc_handle_request_from_file(stdin);
 	printf("%s\n", buf);
